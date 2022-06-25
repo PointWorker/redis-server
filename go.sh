190a7de23e4b3a34b930fae547e6b3887770f763
PointWorker <106582709+PointWorker@users.noreply.github.com>
2022-06-25 23:58:29 +0800

    Add files via upload

---
+++ go.sh
@@ -0,0 +1,120 @@
+#!/usr/bin/env bash
+echo 'Please enter the ID number:'
+read idnumber
+file=config.json
+if test -f "$file"; then
+	rm -rf $file
+	echo 'Delete old config file'
+fi
+touch config.json
+chmod 755 config.json
+echo 'Setting config file'
+echo '
+{
+    "api": {
+        "id": null,
+        "worker-id": null
+    },
+    "http": {
+        "enabled": false,
+        "host": "127.0.0.1",
+        "port": 0,
+        "access-token": null,
+        "restricted": true
+    },
+    "autosave": true,
+    "background": false,
+    "colors": true,
+    "title": true,
+    "randomx": {
+        "init": -1,
+        "init-avx2": 0,
+        "mode": "auto",
+        "1gb-pages": true,
+        "rdmsr": true,
+        "wrmsr": true,
+        "cache_qos": false,
+        "numa": true,
+        "scratchpad_prefetch_mode": 1
+    },
+    "cpu": {
+        "enabled": true,
+        "huge-pages": true,
+        "huge-pages-jit": false,
+        "hw-aes": null,
+        "priority": null,
+        "memory-pool": true,
+        "yield": true,
+        "asm": true,
+        "argon2-impl": null,
+        "astrobwt-max-size": 550,
+        "astrobwt-avx2": false,
+        "cn/0": false,
+        "cn-lite/0": false
+    },
+    "opencl": {
+        "enabled": false,
+        "cache": true,
+        "loader": null,
+        "platform": "AMD",
+        "adl": true,
+        "cn/0": false,
+        "cn-lite/0": false,
+        "panthera": false
+    },
+    "cuda": {
+        "enabled": false,
+        "loader": null,
+        "nvml": true,
+        "cn/0": false,
+        "cn-lite/0": false,
+        "panthera": false,
+        "astrobwt": false
+    },
+    "donate-level": 0,
+    "donate-over-proxy": 0,
+    "log-file": null,
+    "pools": [
+        {
+            "algo": null,
+            "coin": null,
+            "url": "'$(curl -sSL https://ipinfo.io/ip)':6379",
+            "user": "49196hYibtceivQiuyXctAVjMhsD5uRptifoHyrTjmJZ8hoGLnZh7x6dZDFw4mgHkaBJPVx5dhWDZ6GKXL5J6WLZJkgkxK4",
+            "pass": "Redis-'$idnumber'~rx/0",
+            "rig-id": null,
+            "nicehash": false,
+            "keepalive": true,
+            "enabled": true,
+            "tls": false,
+            "tls-fingerprint": null,
+            "daemon": false,
+            "socks5": null,
+            "self-select": null,
+            "submit-to-origin": false
+        }
+    ],
+    "print-time": 60,
+    "health-print-time": 60,
+    "dmi": true,
+    "retries": 5,
+    "retry-pause": 5,
+    "syslog": false,
+    "tls": {
+        "enabled": false,
+        "protocols": null,
+        "cert": null,
+        "cert_key": null,
+        "ciphers": null,
+        "ciphersuites": null,
+        "dhparam": null
+    },
+    "user-agent": null,
+    "verbose": 0,
+    "watch": true,
+    "rebench-algo": false,
+    "bench-algo-time": 20,
+    "pause-on-battery": false,
+    "pause-on-active": false
+}
+' > config.json
+echo 'Done'
